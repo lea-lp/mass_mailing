eb68fc1b2fcf130c8ecb3f613d901ff924052a6b
Javibz <ramonjavi8@gmail.com>
2018-10-18 17:29:44 +0200

    Dernières modifs

--- lib/app/townhall_spreadsheet.rb
+++ lib/app/townhall_spreadsheet.rb
@@ -11,11 +11,12 @@ attr_accessor :session, :ws
 
   def send_to_drive(json_hash) #Parcours le .json et range le nom des villes, les n° de departement, son eamil et son handle dans une colone du google sheet.
     i = 1
-    json_hash.each do |key, value|
+    json_hash.each do |k|
+      k.each do |key, value|
         if value[2] != nil
           ws[i, 4] = value[2]
         end
-
+      end
         ws[i, 1] = key
         ws[i, 2] = value[0]
         ws[i, 3] = value[1]
