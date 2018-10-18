46d977b8f6958ce31a73082c92373f41ac70a54d
Javibz <ramonjavi8@gmail.com>
2018-10-18 16:50:42 +0200

    Branche finie et ajout de commentaires

--- lib/app/townhall_spreadsheet.rb
+++ lib/app/townhall_spreadsheet.rb
@@ -6,10 +6,10 @@ attr_accessor :session, :ws
 
   def initialize
     @session = GoogleDrive::Session.from_config("config.json")
-    @ws = session.spreadsheet_by_key("1W8ESjG5VtdfagOU7fbgneobdE_PmisN7pvileMpNgtc").worksheets[0]
+    @ws = session.spreadsheet_by_key("1yrCKyRpJVQIifVQqdwiWdhtuN0Gw23Z6lSimWcAwAzA").worksheets[0]
   end
 
-  def send_to_drive(json_hash)
+  def send_to_drive(json_hash) #Parcours le .json et range le nom des villes, les n° de departement, son eamil et son handle dans une colone du google sheet.
     i = 1
     json_hash.each do |key, value|
         if value[2] != nil
